a44eba5fe5f76a55d98d4949479db626d91b0c1b
Josh <joshuaagkelley@gmail.com>
2020-09-16 00:57:54 +0100

    Now scrapes data from returned JSON from API, and adds it as a new Tweet.

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ function xhrget(url) {
         // Check status codes
         if (xhttp.readyState == 4 && xhttp.status >= 200) {
             parsedjs = JSON.parse(xhttp.responseText);
-            console.log(parsedjs)
+            formatJson(parsedjs);
         }
     };
     xhttp.send();
@@ -20,7 +20,7 @@ function loadJson() {
     xhrget(url)
     
     
-    writeNewTweet("Steve", "steveMC", "10y", "https://pbs.twimg.com/profile_images/1301535420468072449/su9UImD1.jpg", "Don't dig straight down, you'll regret that.", true);
+    //writeNewTweet("Steve", "steveMC", "10y", "https://pbs.twimg.com/profile_images/1301535420468072449/su9UImD1.jpg", "Don't dig straight down, you'll regret that.", true);
 }
 
 function writeNewTweet(name, screenname, time, iconURL, msg, verified) {
@@ -69,4 +69,49 @@ function writeNewTweet(name, screenname, time, iconURL, msg, verified) {
     
     var twitterFrame = document.getElementsByClassName("twitter-frame")[0];
     twitterFrame.appendChild(innerTweet);
+}
+
+function formatJson(parsedjs) {
+    console.log(parsedjs[0]);
+    var currentElement = parsedjs[0];
+    
+    var name = currentElement.user.name;
+    var screenname = currentElement.user.screen_name;
+    var iconURL = currentElement.user.profile_image_url_https;
+    iconURL = iconURL.replace("_normal", "");
+    var verified = currentElement.user.verified;
+    var msg = currentElement.text;    
+    var time = timeAgo(currentElement.created_at);
+    
+    writeNewTweet(name, screenname, time, iconURL, msg, verified)
+    
+}
+
+
+function timeAgo(timeString) {
+    var epochTime = Date.parse(timeString);
+    var parsedDate = new Date(epochTime);
+    var currentTime = new Date().getTime();
+    var timeDifference = currentTime - epochTime;
+    timeDifference /= 1000 // Time since post in seconds
+    var returnTime = "";
+    
+    if (timeDifference < 60) { // Less than a minute ago
+        returnTime = Math.floor(timeDifference) + "s";
+    } else if (timeDifference < 3600) { // Hour ago
+        returnTime = Math.floor(timeDifference / 60) + "m";
+    } else if (timeDifference < 86400) { // Day ago
+        returnTime = Math.floor(timeDifference / 3600) + "h";
+    } else {
+        var returnMonth = parsedDate.toLocaleString('default', { month: 'short' });
+        var returnDate = parsedDate.getDate();
+        returnTime = returnMonth + " " + returnDate;
+        
+        if (parsedDate.getFullYear() < new Date().getFullYear()) {
+            returnTime += ", " + parsedDate.getFullYear();
+        }
+    }
+    
+    return(returnTime);
+    
 }
